edfa54c53fb8d90522e6e8781271844bbababf2f
Matt Cosentino <matt@learnedmedia.com>
2017-07-13 18:52:10 -0400

    First commit

---
+++ program.js
@@ -0,0 +1,78 @@
+//	// FILTERED LS
+
+	var fs = require("fs");
+
+	var path = require("path");
+
+	var userExtension = "." + process.argv[3];
+
+	fs.readdir(process.argv[2], function doneReading(err, list){
+
+		if( err ) {
+
+			console.log( err );
+
+		} else {
+
+			for (i = 0; i < list.length ; i++) { 
+
+				var fileExtensions = path.extname( list[i] );
+
+				if ( userExtension == fileExtensions ) {
+
+					console.log( list[i] );
+
+				}
+			}
+		}
+
+	});
+
+
+//	// MY FIRST ASYNC I/O
+
+	// var fs = require("fs");
+
+	// fs.readFile(process.argv[2], function doneReading(err, data){
+		
+	// 	if( !err ) {
+	// 		console.log( data.toString().split( "\n" ).length - 1 );
+	// 	} else {
+	// 		console.log( err );
+	// 	}
+
+	// });
+
+// var str = buf.toString();
+
+// var arr = str.split( "\n" );
+
+// console.log( arr.length - 1 );
+
+
+
+//	// MY FIRST I/O
+
+	// var fs = require("fs");
+
+	// var buf = fs.readFileSync(process.argv[2]);
+
+	// var str = buf.toString();
+
+	// var arr = str.split( "\n" );
+
+	// console.log( arr.length - 1 );
+
+
+
+//	// BABY STEPS
+
+	// var total = 0;
+
+	// for (i = 2; i < process.argv.length ; i++) { 
+
+	// 	total += parseInt(process.argv[i]);
+
+	// }
+
+	// console.log( total );
